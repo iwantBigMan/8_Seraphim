ed9333c1d15ded82922788fbd794031de36ad355
iWantBigMan <skarndgus8899@gmail.com>
2023-09-05 18:24:44 +0900

    [FEAT] : ⚙️ 이미지 데이터 추가

--- app/src/main/java/com/android/contactproject/UserDataModel.kt
+++
@@ -1,7 +0,0 @@
-package com.android.contactproject
-
-import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
-
-@Parcelize
-data class UserDataModel(val ph: Int, val name: String) : Parcelable
