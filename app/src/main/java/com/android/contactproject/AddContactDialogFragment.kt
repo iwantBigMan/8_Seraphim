a1f0f13994bafc63e5d39460476fb0e5c80545d4
seongssu <loveforggong@gmail.com>
2023-09-07 21:31:12 +0900

    [CONFLICT] : 💥🚚 conflict해결

--- app/src/main/java/com/android/contactproject/AddContactDialogFragment.kt
+++ app/src/main/java/com/android/contactproject/AddContactDialogFragment.kt
@@ -21,7 +21,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.LinearLayout
-import android.widget.Button
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
