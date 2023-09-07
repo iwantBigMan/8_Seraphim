1174ffcc244e75e6a05c39bc3b98e226341c03f8
seongssu <loveforggong@gmail.com>
2023-09-07 11:17:30 +0900

    [FEAT] : ⚙️ AddContactDialogFragment : 이미지 편집 기능 구현

--- app/src/main/java/com/android/contactproject/AddContactDialogFragment.kt
+++ app/src/main/java/com/android/contactproject/AddContactDialogFragment.kt
@@ -1,20 +1,43 @@
 package com.android.contactproject
 
+import android.app.Activity.RESULT_OK
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.DialogFragment
+import com.android.contactproject.databinding.FragmentAddContactDialogBinding
+import com.bumptech.glide.Glide
 
 
 class AddContactDialogFragment : DialogFragment() {
-
+    private val binding by lazy{ FragmentAddContactDialogBinding.inflate(layoutInflater)}
+    lateinit var addMemberResult : ActivityResultLauncher<Intent>
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_contact_dialog, container, false)
+
+        binding.imageButton.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = "image/*"
+            addMemberResult.launch(intent)
+        }
+        addMemberResult = registerForActivityResult(ActivityResultContracts
+            .StartActivityForResult()){
+            if(it.resultCode == RESULT_OK && it.data !=null){
+                val uri = it.data!!.data
+
+                Glide.with(this)
+                    .load(uri)
+                    .into(binding.dialogImage)
+            }
+        }
+
+        return binding.root
     }
 
 
